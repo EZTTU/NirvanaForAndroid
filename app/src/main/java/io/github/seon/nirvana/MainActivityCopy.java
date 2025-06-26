a016631a6329f1620f6e34a6156df2e458bb10ba
SeonExlike <19150931615@163.com>
2025-06-26 21:25:28 +0800

    feat: Clarify relationship between dp and dpi.

--- app/src/main/java/io/github/seon/nirvana/MainActivityCopy.java
+++ app/src/main/java/io/github/seon/nirvana/MainActivityCopy.java
@@ -1,6 +1,9 @@
 package io.github.seon.nirvana;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,5 +13,14 @@ public class MainActivityCopy extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_copy);
+        TextView colorTest = findViewById(R.id.color_test_textview);
+        colorTest.setTextColor(Color.RED);
+        colorTest.setBackgroundColor(Color.GREEN);
+
+        ViewGroup.LayoutParams layoutParams = colorTest.getLayoutParams();
+        /*
+       px = dip * dpi / 160
+         */
+
     }
 }
