d925c50a04ee8ec7e602660e7280791fca54c0de
zhihua.zhang <zhihua.zhang@carthunder.com>
2025-09-02 18:26:00 +0800

    feat: calculator UI finished.

--- app/src/main/java/io/github/seon/nirvana/calculator/CalculatorActivity.java
+++ app/src/main/java/io/github/seon/nirvana/calculator/CalculatorActivity.java
@@ -1,6 +1,10 @@
 package io.github.seon.nirvana.calculator;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +15,7 @@ import androidx.core.view.WindowInsetsCompat;
 import io.github.seon.nirvana.R;
 
 public class CalculatorActivity extends AppCompatActivity {
+    private TextView tvExpression;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +27,26 @@ public class CalculatorActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        GridLayout buttonField = findViewById(R.id.gl_btn_field);
+
+        for (int i = 0; i < buttonField.getChildCount(); i++) {
+            View childAt = buttonField.getChildAt(i);
+            if (childAt instanceof Button) {
+                Button button = (Button) childAt;
+                switch (button.getText().toString()) {
+                    case "=":
+                        break;
+                    case "C":
+                        break;
+                    case "CE":
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
     }
+
+
 }
